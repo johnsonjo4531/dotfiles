6af63949039e323d6ab2e6c476011110ff492d12
John Johnson II <johnsonjo4531@gmail.com>
2024-09-03 17:20:31 -0600

    Finished typescript support for nvim config with graphql support too

--- neovim/.config/nvim/lua/plugins/jigs.lua
+++ neovim/.config/nvim/lua/plugins/jigs.lua
@@ -68,29 +68,26 @@ return {
   },
 
   -- add pyright to lspconfig
-  {
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        -- pyright will be automatically installed with mason and loaded with lspconfig
-        "pyright",
-        "clangd",
-        "graphql",
-      },
-    },
-  },
-
-  -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
-  -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
-  { import = "lazyvim.plugins.extras.lang.typescript" },
-  { import = "lazyvim.plugins.extras.lang.rust" },
+--  {
+--    "neovim/nvim-lspconfig",
+--    ---@class PluginLspOpts
+--    opts = function(_, opts)
+--      -- add tsx and treesitter
+--      print(opts.servers)
+--      vim.tbl_deep_extend(opts.servers, {
+--        "pyright",
+--        "clangd",
+--        "graphql",
+--      })
+--    end,
+--  },
+  
   -- add more treesitter parsers
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
+    opts = function(_, opts)
+      -- add tsx and treesitter
+      vim.list_extend(opts.ensure_installed, {
         "bash",
         "html",
         "javascript",
@@ -102,27 +99,14 @@ return {
         "query",
         "regex",
         "tsx",
-        "typescript",
         "vim",
         "yaml",
         "graphql",
-      },
-    },
-  },
-
-  -- since `vim.tbl_deep_extend`, can only merge tables and not lists, the code above
-  -- would overwrite `ensure_installed` with the new value.
-  -- If you'd rather extend the default config, use the code below instead:
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      -- add tsx and treesitter
-      vim.list_extend(opts.ensure_installed, {
         "tsx",
         "typescript",
       })
     end,
-  },
+  }, 
 
   {
     "folke/noice.nvim",
@@ -149,16 +133,16 @@ return {
   -- add any tools you want to have installed below
   {
     "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
+    opts = function(_, opts)
+      -- add tsx and treesitter
+      vim.list_extend(opts.ensure_installed, {
         "stylua",
         "shellcheck",
         "shfmt",
         "flake8",
         "graphql-language-service-cli",
-        "graphql-language-service-server",
-      },
-    },
+      })
+    end,
   },
 
   -- Use <tab> for completion and snippets (supertab)
