d11831e8d25330181d1307c1a183afe000ca3641
John David Johnson II <johnsonjo4531@gmail.com>
2024-09-05 07:06:57 -0600

    wezterm updates

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 These are various configuration/dotfiles that I want to use across my machines.
 
 Currently here:
+
 - neovim / lazyvim
 - tmux
 - scripts for my dotfiles (currently only make-dots which restows my dotfiles if needed)
@@ -11,3 +12,17 @@ Currently here:
 
 These dotfiles use GNU Stow
 
+## Required binaries to install dotfiles
+
+- git
+- stow
+
+## Binaries
+
+- nvim (neovim)
+- tmux
+- [sesh](https://github.com/joshmedeski/sesh)
+- [wezterm](https://github.com/wez/wezterm?tab=readme-ov-file)
+- [fzf](https://github.com/junegunn/fzf)
+- [bat](https://github.com/sharkdp/bat)
+-
--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,74 +1,32 @@
 local wezterm = require("wezterm")
--- Replace the old wezterm.on('update-status', ... function with this:
+local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
 
-local function segments_for_right_status(window)
-	return {
-		window:active_workspace(),
-		wezterm.strftime("%a %b %-d %H:%M"),
-		wezterm.hostname(),
-	}
+tabline.setup()
+
+local function spread(template)
+	return function(table)
+		local result = {}
+		for key, value in pairs(template) do
+			result[key] = value
+		end
+
+		for key, value in pairs(table) do
+			result[key] = value
+		end
+		return result
+	end
 end
 
-wezterm.on("update-status", function(window, _)
-	local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
-	local segments = segments_for_right_status(window)
-
-	local color_scheme = window:effective_config().resolved_palette
-	-- Note the use of wezterm.color.parse here, this returns
-	-- a Color object, which comes with functionality for lightening
-	-- or darkening the colour (amongst other things).
-	local bg = wezterm.color.parse(color_scheme.background)
-	local fg = color_scheme.foreground
-
-	-- Each powerline segment is going to be coloured progressively
-	-- darker/lighter depending on whether we're on a dark/light colour
-	-- scheme. Let's establish the "from" and "to" bounds of our gradient.
-	local gradient_to, gradient_from = bg
-	if appearance.is_dark() then
-		gradient_from = gradient_to:lighten(0.2)
-	else
-		gradient_from = gradient_to:darken(0.2)
-	end
-
-	-- Yes, WezTerm supports creating gradients, because why not?! Although
-	-- they'd usually be used for setting high fidelity gradients on your terminal's
-	-- background, we'll use them here to give us a sample of the powerline segment
-	-- colours we need.
-	local gradient = wezterm.color.gradient(
-		{
-			orientation = "Horizontal",
-			colors = { gradient_from, gradient_to },
-		},
-		#segments -- only gives us as many colours as we have segments.
-	)
-
-	-- We'll build up the elements to send to wezterm.format in this table.
-	local elements = {}
-
-	for i, seg in ipairs(segments) do
-		local is_first = i == 1
-
-		if is_first then
-			table.insert(elements, { Background = { Color = "none" } })
-		end
-		table.insert(elements, { Foreground = { Color = gradient[i] } })
-		table.insert(elements, { Text = SOLID_LEFT_ARROW })
-
-		table.insert(elements, { Foreground = { Color = fg } })
-		table.insert(elements, { Background = { Color = gradient[i] } })
-		table.insert(elements, { Text = " " .. seg .. " " })
-	end
-
-	window:set_right_status(wezterm.format(elements))
-end)
-
-return {
-	-- color_scheme = 'termnial.sexy',
+local config = spread(wezterm.config_builder())({
 	color_scheme = "Catppuccin Mocha",
 	enable_tab_bar = true,
 	use_fancy_tab_bar = true,
 	font_size = 14.0,
 	font = wezterm.font("FiraCode Nerd Font"),
+	window_frame = {
+		font = wezterm.font("FiraCode Nerd Font"),
+		size = 13,
+	},
 	window_padding = {
 		top = 0,
 		bottom = 0,
@@ -84,7 +42,7 @@ return {
 	-- 	saturation = 0.5,
 	-- },
 	-- window_background_opacity = 0.92,
-	window_background_opacity = 0.8,
+	window_background_opacity = 0.95,
 	-- window_background_opacity = 0.78,
 	-- window_background_opacity = 0.20,
 	window_decorations = "RESIZE",
@@ -103,4 +61,8 @@ return {
 			action = wezterm.action.OpenLinkAtMouseCursor,
 		},
 	},
-}
+})
+
+tabline.apply_to_config(config)
+
+return config
