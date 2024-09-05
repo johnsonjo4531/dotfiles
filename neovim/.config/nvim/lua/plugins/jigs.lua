36da4c5395b7b0c869bb3677648b91b34731e8a1
John David Johnson II <johnsonjo4531@gmail.com>
2024-09-05 09:01:17 -0600

    Fixed submodule not initing

--- neovim/.config/nvim/lua/plugins/jigs.lua
+++ neovim/.config/nvim/lua/plugins/jigs.lua
@@ -146,6 +146,27 @@ return {
     end,
   }, 
 
+
+  {
+    'nvim-neotest/neotest',
+    dependencies = {
+      'thenbe/neotest-playwright',
+      dependencies = 'nvim-telescope/telescope.nvim',
+    },
+    config = function()
+      require('neotest').setup({
+        adapters = {
+          require('neotest-playwright').adapter({
+            options = {
+              persist_project_selection = true,
+              enable_dynamic_test_discovery = true,
+            },
+          }),
+        },
+      })
+    end,
+  },
+
   {
     "folke/noice.nvim",
     opts = {
