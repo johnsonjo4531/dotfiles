3bf26526bbf76f12b48db051c00cf85baa495d19
John D. Johnson II <johnsonjo4531@gmail.com>
2025-03-13 17:17:32 -0600

    Updating lazyvim 💤

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
--- Terminal
-vim.keymap.set("n", "<C-LeftMouse>", "<C-w>gf", { desc = "Open File Under Cursor" })
