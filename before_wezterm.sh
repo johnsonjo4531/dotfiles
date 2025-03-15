afd759124db3b2bef1ca332c8992d8995272c1ff
John D. Johnson II <johnsonjo4531@gmail.com>
2025-03-15 13:15:52 -0600

    sesh updates

--- before_wezterm.sh
+++ before_wezterm.sh
@@ -1,6 +1,6 @@
 # shellcheck shell=bash
 
-# -f checks whether or not the file exists
+# -v checks whether or not the variable exists
 if [[ -v TMUX ]] && [[ -v NVIM ]]; then
   export WEZTERM_SHELL_SKIP_ALL="1"
 fi
--- install.sh
+++ install.sh
@@ -2,5 +2,8 @@
 
 # patch for this issue running nvim in tmux in wezterm: https://github.com/wezterm/wezterm/issues/5007
 if [[ -f /etc/profile.d/wezterm.sh ]]; then
-  sudo cp ./before_wezterm.sh /etc/profile.d/before_wezterm.sh
+  # Check if the file does not already exist
+  if [ ! -f /etc/profile.d/before_wezterm.sh ]; then
+    sudo cp ./before_wezterm.sh /etc/profile.d/before_wezterm.sh
+  fi
 fi
